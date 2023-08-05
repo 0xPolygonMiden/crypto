b3e7578ab228c82936a57ae98e7d62987d4357aa
Bobbin Threadbare <bobbinth@protonmail.com>
2023-08-04 22:46:23 -0700

    fix: misspelled variant name in TieredSmtProofError

--- src/merkle/tiered_smt/error.rs
+++ src/merkle/tiered_smt/error.rs
@@ -3,11 +3,11 @@ use core::fmt::Display;
 #[derive(Debug, PartialEq, Eq)]
 pub enum TieredSmtProofError {
     EntriesEmpty,
-    PathTooLong,
-    NotATierPath(u8),
-    MultipleEntriesOutsideLastTier,
     EmptyValueNotAllowed,
-    UnmatchingPrefixes(u64, u64),
+    MismatchedPrefixes(u64, u64),
+    MultipleEntriesOutsideLastTier,
+    NotATierPath(u8),
+    PathTooLong,
 }
 
 impl Display for TieredSmtProofError {
@@ -16,30 +16,29 @@ impl Display for TieredSmtProofError {
             TieredSmtProofError::EntriesEmpty => {
                 write!(f, "Missing entries for tiered sparse merkle tree proof")
             }
+            TieredSmtProofError::EmptyValueNotAllowed => {
+                write!(
+                    f,
+                    "The empty value [0, 0, 0, 0] is not allowed inside a tiered sparse merkle tree"
+                )
+            }
+            TieredSmtProofError::MismatchedPrefixes(first, second) => {
+                write!(f, "Not all leaves have the same prefix. First {first} second {second}")
+            }
+            TieredSmtProofError::MultipleEntriesOutsideLastTier => {
+                write!(f, "Multiple entries are only allowed for the last tier (depth 64)")
+            }
+            TieredSmtProofError::NotATierPath(got) => {
+                write!(
+                    f,
+                    "Path length does not correspond to a tier. Got {got} Expected one of 16, 32, 48, 64"
+                )
+            }
             TieredSmtProofError::PathTooLong => {
                 write!(
                     f,
                     "Path longer than maximum depth of 64 for tiered sparse merkle tree proof"
                 )
-            }
-            TieredSmtProofError::NotATierPath(got) => {
-                write!(
-                    f,
-                    "Path length does not correspond to a tier. Got {} Expected one of 16,32,48,64",
-                    got
-                )
-            }
-            TieredSmtProofError::MultipleEntriesOutsideLastTier => {
-                write!(f, "Multiple entries are only allowed for the last tier (depth 64)")
-            }
-            TieredSmtProofError::EmptyValueNotAllowed => {
-                write!(
-                    f,
-                    "The empty value [0,0,0,0] is not allowed inside a tiered sparse merkle tree"
-                )
-            }
-            TieredSmtProofError::UnmatchingPrefixes(first, second) => {
-                write!(f, "Not all leaves have the same prefix. First {} second {}", first, second)
             }
         }
     }
