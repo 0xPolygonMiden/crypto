01be4d6b9d0b0bbb50623b96e26c18f6030e0368
Grzegorz Swirski <grzegorz@swirski.name>
2023-09-24 22:23:38 +0200

    refactor: move arch specific code to rpo folder, don't run SVE on CI

--- build.rs
+++ build.rs
@@ -5,12 +5,12 @@ fn main() {
 
 #[cfg(feature = "arch-arm64-sve")]
 fn compile_arch_arm64_sve() {
-    println!("cargo:rerun-if-changed=arch/arm64-sve/library.c");
-    println!("cargo:rerun-if-changed=arch/arm64-sve/library.h");
-    println!("cargo:rerun-if-changed=arch/arm64-sve/rpo_hash.h");
+    println!("cargo:rerun-if-changed=arch/arm64-sve/rpo/library.c");
+    println!("cargo:rerun-if-changed=arch/arm64-sve/rpo/library.h");
+    println!("cargo:rerun-if-changed=arch/arm64-sve/rpo/rpo_hash.h");
 
     cc::Build::new()
-        .file("arch/arm64-sve/library.c")
+        .file("arch/arm64-sve/rpo/library.c")
         .flag("-march=armv8-a+sve")
         .flag("-O3")
         .compile("rpo_sve");
