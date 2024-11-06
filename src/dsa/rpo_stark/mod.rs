cb15287ce7e3e4766d41f2154b47395d81131d98
Al-Kindi-0 <82364884+Al-Kindi-0@users.noreply.github.com>
2024-11-06 15:02:09 +0100

    fix: clippy

--- src/dsa/rpo_stark/mod.rs
+++ src/dsa/rpo_stark/mod.rs
@@ -8,13 +8,18 @@ mod stark;
 
 #[cfg(test)]
 mod tests {
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha20Rng;
+
     use super::SecretKey;
 
     #[test]
     fn test_signature() {
         use rand_utils::rand_array;
 
-        let sk = SecretKey::new();
+        let seed = [0_u8; 32];
+        let mut rng = ChaCha20Rng::from_seed(seed);
+        let sk = SecretKey::with_rng(&mut rng);
 
         let message = rand_array();
         let signature = sk.sign(message);
