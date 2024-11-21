a27f9ad8280f4a926b5d85d9ea15dbb76ab9277d
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2024-11-22 00:52:20 +0100

    refactor: use `thiserror` to derive errors and update error messages (#344)

--- src/merkle/error.rs
+++ src/merkle/error.rs
@@ -1,65 +1,34 @@
-use alloc::vec::Vec;
-use core::fmt;
+use thiserror::Error;
 
-use super::{smt::SmtLeafError, MerklePath, NodeIndex, RpoDigest};
+use super::{NodeIndex, RpoDigest};
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Debug, Error)]
 pub enum MerkleError {
-    ConflictingRoots(Vec<RpoDigest>),
+    #[error("expected merkle root {expected_root} found {actual_root}")]
+    ConflictingRoots {
+        expected_root: RpoDigest,
+        actual_root: RpoDigest,
+    },
+    #[error("provided merkle tree depth {0} is too small")]
     DepthTooSmall(u8),
+    #[error("provided merkle tree depth {0} is too big")]
     DepthTooBig(u64),
+    #[error("multiple values provided for merkle tree index {0}")]
     DuplicateValuesForIndex(u64),
-    DuplicateValuesForKey(RpoDigest),
-    InvalidIndex { depth: u8, value: u64 },
-    InvalidDepth { expected: u8, provided: u8 },
-    InvalidSubtreeDepth { subtree_depth: u8, tree_depth: u8 },
-    InvalidPath(MerklePath),
-    InvalidNumEntries(usize),
-    NodeNotInSet(NodeIndex),
-    NodeNotInStore(RpoDigest, NodeIndex),
+    #[error("node index value {value} is not valid for depth {depth}")]
+    InvalidNodeIndex { depth: u8, value: u64 },
+    #[error("provided node index depth {provided} does not match expected depth {expected}")]
+    InvalidNodeIndexDepth { expected: u8, provided: u8 },
+    #[error("merkle subtree depth {subtree_depth} exceeds merkle tree depth {tree_depth}")]
+    SubtreeDepthExceedsDepth { subtree_depth: u8, tree_depth: u8 },
+    #[error("number of entries in the merkle tree exceeds the maximum of {0}")]
+    TooManyEntries(usize),
+    #[error("node index `{0}` not found in the tree")]
+    NodeIndexNotFoundInTree(NodeIndex),
+    #[error("node {0:?} with index `{1}` not found in the store")]
+    NodeIndexNotFoundInStore(RpoDigest, NodeIndex),
+    #[error("number of provided merkle tree leaves {0} is not a power of two")]
     NumLeavesNotPowerOfTwo(usize),
+    #[error("root {0:?} is not in the store")]
     RootNotInStore(RpoDigest),
-    SmtLeaf(SmtLeafError),
-}
-
-impl fmt::Display for MerkleError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use MerkleError::*;
-        match self {
-            ConflictingRoots(roots) => write!(f, "the merkle paths roots do not match {roots:?}"),
-            DepthTooSmall(depth) => write!(f, "the provided depth {depth} is too small"),
-            DepthTooBig(depth) => write!(f, "the provided depth {depth} is too big"),
-            DuplicateValuesForIndex(key) => write!(f, "multiple values provided for key {key}"),
-            DuplicateValuesForKey(key) => write!(f, "multiple values provided for key {key}"),
-            InvalidIndex { depth, value } => {
-                write!(f, "the index value {value} is not valid for the depth {depth}")
-            },
-            InvalidDepth { expected, provided } => {
-                write!(f, "the provided depth {provided} is not valid for {expected}")
-            },
-            InvalidSubtreeDepth { subtree_depth, tree_depth } => {
-                write!(f, "tried inserting a subtree of depth {subtree_depth} into a tree of depth {tree_depth}")
-            },
-            InvalidPath(_path) => write!(f, "the provided path is not valid"),
-            InvalidNumEntries(max) => write!(f, "number of entries exceeded the maximum: {max}"),
-            NodeNotInSet(index) => write!(f, "the node with index ({index}) is not in the set"),
-            NodeNotInStore(hash, index) => {
-                write!(f, "the node {hash:?} with index ({index}) is not in the store")
-            },
-            NumLeavesNotPowerOfTwo(leaves) => {
-                write!(f, "the leaves count {leaves} is not a power of 2")
-            },
-            RootNotInStore(root) => write!(f, "the root {:?} is not in the store", root),
-            SmtLeaf(smt_leaf_error) => write!(f, "smt leaf error: {smt_leaf_error}"),
-        }
-    }
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for MerkleError {}
-
-impl From<SmtLeafError> for MerkleError {
-    fn from(value: SmtLeafError) -> Self {
-        Self::SmtLeaf(value)
-    }
 }
