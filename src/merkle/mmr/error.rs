0b09eb35af0c0e15707a85d704cbec41514d1c46
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2024-11-21 16:20:25 +0100

    feat: Use `thiserror` for `MmrError`

--- src/merkle/mmr/error.rs
+++ src/merkle/mmr/error.rs
@@ -1,41 +1,27 @@
-use core::fmt::{Display, Formatter};
-#[cfg(feature = "std")]
-use std::error::Error;
+use alloc::string::String;
+
+use thiserror::Error;
 
 use crate::merkle::MerkleError;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum MmrError {
-    InvalidPosition(usize),
-    InvalidPeaks,
-    InvalidPeak,
-    PeakOutOfBounds(usize, usize),
+    #[error("mmr does not contain position {0}")]
+    PositionNotFound(usize),
+    #[error("mmr peaks are invalid: {0}")]
+    InvalidPeaks(String),
+    #[error(
+        "mmr peak does not match the computed merkle root of the provided authentication path"
+    )]
+    PeakPathMismatch,
+    #[error("requested peak index is {peak_idx} but the number of peaks is {peaks_len}")]
+    PeakOutOfBounds { peak_idx: usize, peaks_len: usize },
+    #[error("invalid mmr update")]
     InvalidUpdate,
-    UnknownPeak,
-    MerkleError(MerkleError),
+    #[error("mmr does not contain a peak with depth {0}")]
+    UnknownPeak(u8),
+    #[error("invalid merkle path")]
+    InvalidMerklePath(#[source] MerkleError),
+    #[error("merkle root computation failed")]
+    MerkleRootComputationFailed(#[source] MerkleError),
 }
-
-impl Display for MmrError {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), core::fmt::Error> {
-        match self {
-            MmrError::InvalidPosition(pos) => write!(fmt, "Mmr does not contain position {pos}"),
-            MmrError::InvalidPeaks => write!(fmt, "Invalid peaks count"),
-            MmrError::InvalidPeak => {
-                write!(fmt, "Peak values does not match merkle path computed root")
-            },
-            MmrError::PeakOutOfBounds(peak_idx, peaks_len) => write!(
-                fmt,
-                "Requested peak index is {} but the number of peaks is {}",
-                peak_idx, peaks_len
-            ),
-            MmrError::InvalidUpdate => write!(fmt, "Invalid Mmr update"),
-            MmrError::UnknownPeak => {
-                write!(fmt, "Peak not in Mmr")
-            },
-            MmrError::MerkleError(err) => write!(fmt, "{}", err),
-        }
-    }
-}
-
-#[cfg(feature = "std")]
-impl Error for MmrError {}
