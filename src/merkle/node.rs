da9bc922f7e91c18fd5c08d4cd68d0c3951b4822
polydez <155382956+polydez@users.noreply.github.com>
2024-12-29 11:14:31 +0500

    refactor: make `InnerNodeInfo` derive `PartialOrd, Ord` only for tests

--- src/merkle/node.rs
+++ src/merkle/node.rs
@@ -1,8 +1,9 @@
 use super::RpoDigest;
 
 /// Representation of a node with two children used for iterating over containers.
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(test, derive(PartialOrd, Ord))]
 pub struct InnerNodeInfo {
     pub value: RpoDigest,
     pub left: RpoDigest,
